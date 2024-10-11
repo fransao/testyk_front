8996b1df8e335a00411f006bcb4471be7b457632
DESKTOP-DG1O2KF\Company <francisco-salazar8a@hotmail.com>
2024-10-11 08:11:10 -0500

    added endpoints

--- test_yk_front/src/app/api.service.ts
+++ test_yk_front/src/app/api.service.ts
@@ -9,9 +9,15 @@ import { Test } from './components/test/Test';
 
 export class ApiService {
 
+  /* test soapui
   private getTestsUrl     = 'http://localhost:8080/tests';
   private getQuestionsUrl = 'http://localhost:8080/questions';
   private putAnswersUrl   = 'http://localhost:8080/answers';
+  */
+
+  private getTestsUrl     = 'http://localhost:8080/course/user/tests?userID=1234';
+  private getQuestionsUrl = 'http://localhost:8080/course/answers/question?questionID=1';
+  private postAnswersUrl  = 'http://localhost:8080/course/validate/answers/test';
   
   constructor(private http: HttpClient) {}
 
@@ -40,6 +46,6 @@ export class ApiService {
   }
 
   postAnswers(data: Test[]): Observable<Test[]> {
-    return this.http.post<Test[]>(`${this.putAnswersUrl}`, data)
+    return this.http.post<Test[]>(`${this.postAnswersUrl}`, data)
   }
 }
